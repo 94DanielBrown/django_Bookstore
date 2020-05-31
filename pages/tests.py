da2f067e819ee8c0c0e0b7c2a29a3d8d08a4d84c
Daniel Brown <daniel.brown@trakcel.com>
2020-05-31 08:52:45 +0100

    Added homepage view test

--- pages/tests.py
+++ pages/tests.py
@@ -1,27 +1,34 @@
 import pytest
-from django.urls import reverse
+from django.urls import reverse, resolve
+from .views import HomePageView
 
 
-def test_homepage_status_code(client):
-    response = client.get('/')
-    assert response.status_code == 200
-
-
-def test_homepage_url_name(client):
+@pytest.fixture
+def response(client):
     response = client.get(reverse('home'))
+    return response
+
+
+def test_homepage_status_code(response):
     assert response.status_code == 200
 
 
-def test_homepage_template(client):
-    response = client.get('/')
+def test_homepage_url_name(response):
+    assert response.status_code == 200
+
+
+def test_homepage_template(response):
     assert 'home.html' in (t.name for t in response.templates)
 
 
-def test_homepage_contains_correct_html(client):
-    response = client.get('/')
+def test_homepage_contains_correct_html(response):
     assert 'Homepage' in str(response.content)
 
 
-def test_homepage_does_not_contain_incorrect_html(client):
-    response = client.get('/')
+def test_homepage_does_not_contain_incorrect_html(response):
     assert 'Some silly sentence' not in str(response.content)
+
+
+def test_homepage_url_resolves_homepageview():
+    view = resolve('/')
+    assert view.func.__name__ == HomePageView.as_view().__name__
